64196815773d9107fc04c58b3d6e680a5d82690c
MSH <maxim.shustov@gmail.com>
2020-08-09 13:21:16 +0300

    5-й коммит, добавлен основной script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+"use strict";
+
+let numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+};
+
+const a = prompt("Один из последних просмотренных фильмов?", "");
+const b = prompt("На сколько оцените его?", "");
+const c = prompt("Один из последних просмотренных фильмов?", "");
+const d = prompt("На сколько оцените его?", "");
+
+personalMovieDB.movies = {[a]: b, [c]: d};
+
+console.log(personalMovieDB);
